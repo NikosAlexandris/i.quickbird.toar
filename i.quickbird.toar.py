1046b8744100525ff4811575a82b15836bd1af35
Nikos Alexandris <nik@nikosalexandris.net>
2014-11-17 21:59:49 +0200

    Fixed. Added info flag. Allow mapcalc to overwrite tmp files.

--- i.quickbird.toar.py
+++ i.quickbird.toar.py
@@ -8,7 +8,7 @@ AUTHOR:         Nikos Alexandris <nik@nikosalexandris.net>
                 Converted from a bash shell script | Trikala, November 2014
 
 
-PURPOSE:        Converting QuickBird2 DN values to Spectral Radiance or 
+PURPOSE:        Converting QuickBird2 DN values to Spectral Radiance or
                     Reflectance
 
 
@@ -31,7 +31,7 @@ PURPOSE:        Converting QuickBird2 DN values to Spectral Radiance or
 
                 Sources -----------------------------------------------------
 
-                Radiometric Use of QuickBird Imagery, Technical Note. 
+                Radiometric Use of QuickBird Imagery, Technical Note.
                 2005-11-07, by Keith Krause.
 
 
@@ -55,6 +55,11 @@ PURPOSE:        Converting QuickBird2 DN values to Spectral Radiance or
 #%flag
 #%  key: k
 #%  description: Keep current computational region settings
+#%end
+
+#%flag
+#%  key: i
+#%  description: Print out coversion formulas (Krause, 2005)
 #%end
 
 #%option G_OPT_R_INPUTS
@@ -92,7 +97,7 @@ PURPOSE:        Converting QuickBird2 DN values to Spectral Radiance or
 #% key_desc: day of year
 #% type: integer
 #% label: Day of Year
-#% description: User defined aquisition's Day of Year (Julian Day) to calculate Earth-Sun distance
+#% description: User defined aquisition's Day of Year (Julian Day) to calculate Earth-Sun distance. Will override UTC string input.
 #% options: 1-365
 #% guisection: Metadata
 #% required: no
@@ -123,6 +128,8 @@ PURPOSE:        Converting QuickBird2 DN values to Spectral Radiance or
 # required librairies -------------------------------------------------------
 import os
 import sys
+sys.path.insert(1, os.path.join(os.path.dirname(sys.path[0]),
+                                'etc', 'i.quickbird.toar'))
 
 import atexit
 import grass.script as grass
@@ -139,7 +146,7 @@ tmp_rad = ''
 tmp_toar = ''
 
 
-# constants
+# constants ----------------------------------------------------------------
 """
 QuickBird2 Band-Averaged Solar Spectral Irradiance [W/sq.m./μm]
 
@@ -180,16 +187,11 @@ KCF = {
 
 spectral_bands = KCF.keys()
 
-
 # string for metadata
 source1_rad = source1_toar = '"Radiometric Use of QuickBird Imagery, '
 'Technical Note (2005)," by Keith Krause, Digital Globe'
 source2_rad = source2_toar = ""  # Add some source2?
 
-
-# Aquisition's MetaData -----------------------------------------------------
-
-#?
 
 # helper functions ----------------------------------------------------------
 def cleanup():
@@ -214,8 +216,24 @@ def main():
     doy = options['doy']
     sea = options['sea']
     tdi = options['tdi']
+
     radiance = flags['r']
+    if radiance and outputsuffix == 'toar':
+        outputsuffix = 'rad'
+        g.message("Output-suffix set to %s" % outputsuffix)
+
     keep_region = flags['k']
+    info = flags['i']
+
+    # -----------------------------------------------------------------------
+    # Equations
+    # -----------------------------------------------------------------------
+
+    if info:
+        # conversion to Radiance based on (1)
+        msg = "|i Spectral Radiance = K * DN / Effective Bandwidth | " \
+              "Reflectance = ( Pi * Radiance * ESD^2 ) / BAND_Esun * cos(SZA)"
+        g.message(msg)
 
     # -----------------------------------------------------------------------
     # List images and their properties
@@ -265,14 +283,21 @@ def main():
     # Loop processing over all bands
     # -----------------------------------------------------------------------
     for band in spectral_bands:
-        
+
         global tmp_rad
 
-        # Why is this necessary?  Any function to remove the mapsets name?
-        if '@' in band:
-            band_key = (band.split('@')[0])
-        else:
-            band_key = band
+        # -------------------------------------------------------------------
+        # Match bands region if... ?
+        # -------------------------------------------------------------------
+
+        if not keep_region:
+            run('g.region', rast=band)   # ## FixMe?
+            msg = "\n|! Region matching the %s spectral band" % band
+            g.message(msg)
+
+        elif keep_region:
+            msg = "|! Operating on current region"
+            g.message(msg)
 
         # -------------------------------------------------------------------
         # Band dependent metadata for Spectral Radiance
@@ -281,13 +306,18 @@ def main():
         # which Pan TDI level?
         if band == 'Pan':
             band_key = band + tdi
-            g.message("Processing for %s" % pantdi)
+            g.message("\n|* Processing the Panchromatic band (TDI level: %s)"
+                      % tdi, flags='i')
         else:
             band_key = band
+            # some echo...
+            g.message("\n|* Processing the %s band"
+                      % band, flags='i')
 
-        # some echo...
-        g.message("Processing the %s spectral band" % band)
-    
+        # Why is this necessary?  Any function to remove the mapsets name?
+        if '@' in band:
+            band_key = (band.split('@')[0])
+
         # check bitness
         if 0 <= images[band].max <= 255:
             # 8-bit
@@ -298,48 +328,31 @@ def main():
             kcf = float(KCF[band_key][2])
             kcf_msg = "K=" + str(kcf)
 
-        # effective bandwidth        
+        # effective bandwidth
         bw = float(KCF[band_key][0])
-
-        # -------------------------------------------------------------------
-        # Match bands region if... ?
-        # -------------------------------------------------------------------
-
-        if not keep_region:
-            run('g.region', rast=band)   # ## FixMe?
-            msg = "Region matching the %s spectral band" % band
-            g.message(msg)
-
-        elif keep_region:
-            msg = "|! Operating on current region!"
-            g.message(msg)
 
         # -------------------------------------------------------------------
         # Converting to Spectral Radiance
         # -------------------------------------------------------------------
 
-        # inform
-        msg ="Band Parameters set to %s, Bandwidth=%.3f" % (kcf_msg, bw)
+        msg = "\n|> Converting to Spectral Radiance " \
+              "| Conversion Factor %s, Bandwidth=%.3f" % (kcf_msg, bw)
         g.message(msg)
 
-        # conversion to Radiance based on (1) 
-        msg = "Radiance = K * DN / Bandwidth"
-        g.message(msg)
-
-        # convert    
-        tmp_rad = "%s.Radiance" % tmp  # Temporary Ma
+        # convert
+        tmp_rad = "%s.Radiance" % tmp  # Temporary Map
         rad = "%s = %f * %s / %f" \
-            % (tmp_rad, kcf, band, bw)  # Attention: 32-bit calculations required
-        grass.mapcalc(rad)
-        
-        
+            % (tmp_rad, kcf, band, bw)  # Attention: 32-bit calculations requ.
+        grass.mapcalc(rad, overwrite=True)
+
         # strings for metadata
         history_rad = rad
         history_rad += "Conversion Factor=%f; Effective Bandwidth=%.3f" \
-            % (kcf, bw)            
-        title_rad=""
-        description_rad="Top-of-Atmosphere %s band spectral Radiance [W/m^2/sr/μm]" % band
-        units_rad="W / sq.m. / μm / ster"
+            % (kcf, bw)
+        title_rad = ""
+        description_rad = "Top-of-Atmosphere %s band spectral Radiance " \
+                          "[W/m^2/sr/μm]" % band
+        units_rad = "W / sq.m. / μm / ster"
 
         if not radiance:
 
@@ -348,52 +361,49 @@ def main():
             # ---------------------------------------------------------------
 
             global tmp_toar
-            
+
+            msg = "\n|> Converting to Top-of-Atmosphere Reflectance"
+            g.message(msg)
+
             esun = float(QB2ESUN[band_key])
-            msg = "Using Esun = %.2f" % esun
+            msg = "   %s band mean solar exoatmospheric irradiance=%.2f" \
+                % (band_key, esun)
             g.message(msg)
-    
-            # calculate ToAR  -- tmp_rad is already 32-bit -- see above!
-            msg = "Reflectance = ( math.pi * Radiance * ESD^2 ) / BAND_Esun * cos(SZA)"
-            g.message(msg)
-            
+
             # convert
             tmp_toar = "%s.Reflectance" % tmp  # Spectral Reflectance
             toar = "%s = %f * %s * %f^2 / %f * %f" \
                 % (tmp_toar, math.pi, tmp_rad, esd, esun, sza)
-            grass.mapcalc(toar)
-            print tmp_toar, "TMP_TOAR is here!!!"
+            grass.mapcalc(toar, overwrite=True)
 
             # strings for metadata
-            title_toar="%s band (Top of Atmosphere Reflectance)" % band
-            description_toar="Top of Atmosphere %s band spectral Reflectance" \
+            title_toar = "%s band (Top of Atmosphere Reflectance)" % band
+            description_toar = "Top of Atmosphere %s band spectral Reflectance" \
                 % band
-            units_toar="Unitless planetary reflectance"
-            history_toar="K=%f; Bandwidth=%.1f; ESD=%f; Esun=%.2f; SZA=%.1f" \
+            units_toar = "Unitless planetary reflectance"
+            history_toar = "K=%f; Bandwidth=%.1f; ESD=%f; Esun=%.2f; SZA=%.1f" \
                 % (kcf, bw, esd, esun, sza)
 
         if tmp_toar:
-    
+
             # history entry
-            run("r.support", map=tmp_toar,
-                title=title_toar, units=units_toar, description=description_toar,
-                source1=source1_toar, source2=source2_toar, history=history_toar)
-    
+            run("r.support", map=tmp_toar, title=title_toar,
+                units=units_toar, description=description_toar,
+                source1=source1_toar, source2=source2_toar,
+                history=history_toar)
+
             # add suffix to basename & rename end product
-    #        toar_name = ("%s.%s" % (band, outputsuffix))
             toar_name = ("%s.%s" % (band.split('@')[0], outputsuffix))
             run("g.rename", rast=(tmp_toar, toar_name))
-            print tmp_toar, "\nAND HERE ****************************************\n"
-    
+
         elif tmp_rad:
-    
+
             # history entry
             run("r.support", map=tmp_rad,
                 title=title_rad, units=units_rad, description=description_rad,
                 source1=source1_rad, source2=source2_rad, history=history_rad)
-    
-            # add suffix to basename & rename end product        
-    #        rad_name = ("%s.%s" % (band, outputsuffix))
+
+            # add suffix to basename & rename end product
             rad_name = ("%s.%s" % (band.split('@')[0], outputsuffix))
             run("g.rename", rast=(tmp_rad, rad_name))
 
